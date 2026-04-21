680d8c5100706c5f7c4cf2ef314f8bd061000ada
Farolito <98248758+Ansiedad07@users.noreply.github.com>
2026-04-21 09:09:24 -0600

    Mejoras a tdaPtr

--- tdaPtr.c
+++ tdaPtr.c
@@ -22,7 +22,7 @@ int menu(void){
 	return opcion;
 }
 
-void crearDato(struct Dato **ptr);
+struct Dato* crearDato(void);
 void mostrarDato(struct Dato *ptr);
 void liberarDato(struct Dato **ptr);
 
@@ -34,7 +34,17 @@ int main (void){
 		opcion = menu();
 		switch(opcion){
 			case 1:
-				// Crear dato
+				ptr = crearDato();
+				if(ptr == NULL){
+					printf("No se pudo crear el dato.\n");
+				} else {
+					printf("Dato creado exitosamente.\n");
+					if(ptr == NULL){
+						//ptr = ptrTemp;
+					} else {
+
+					}
+				}
 				break;
 			case 2:
 				// Mostrar dato
@@ -56,14 +66,18 @@ int main (void){
  * 1.- La funcion crea el nuevo modulo y retorna la nueva direccion de memoria de ese modulo de tal forma como ptr = crearDato(ptr);
  * 2.- La direccion de la variable ptr e implementa doble apuntador para modificar ptr desde la funcion, es decir, crearDato(&ptr);
  */
-void crearDato(struct Dato **ptr){
+struct Dato * crearDato(void){
 	struct Dato *ptrTemp; // Se crea una variable temporal (es una memoria estatica, se destruye al salir de la función)
 	ptrTemp = (struct Dato *)malloc(sizeof(struct Dato)); // Se asigna memoria dinámica para un dato (este bloque no se destruye)
 	if(ptrTemp == NULL){
 		printf("Error al asignar memoria.\n");
-		return;
+		return NULL;
+	} else {
+		printf("Ingrese un entero: ");
+		scanf("%d", &ptrTemp->d); // El operador -> se utiliza para acceder a los miembros de una estructura a través de un apuntador
+		ptrTemp->ptrSig = NULL; // El siguiente dato apunta a NULL, indicando que es el último dato de la lista
+		return ptrTemp; // Retorna la dirección del nuevo dato creado
 	}
-	*ptr = ptrTemp; // Se asigna la nueva direccion de memoria al puntero original (ptr)
 }
 
 void mostrarDato(struct Dato *ptr){
@@ -71,3 +85,7 @@ void mostrarDato(struct Dato *ptr){
 
 void liberarDato(struct Dato **ptr){
 }
+
+/**
+ * Si ves esto prometo mejorarlo xd
+ */
